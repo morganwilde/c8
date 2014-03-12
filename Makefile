90e87293bade52d2c94f8386f58ef97f3c0dcc0f
Morgan Wilde (Uni) <wildemorgan@gmail.com>
2014-03-12 16:46:15 +0200

    third task

---
+++ Makefile
@@ -0,0 +1,2 @@
+lecture: lecture.c reader.c utils.c
+	gcc lecture.c reader.c utils.c -lm -o lecture
--- lecture.c
+++ lecture.c
@@ -1,15 +1,44 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include "reader.h" // Contains readIntReturn();
 
 int main(void) {
 
-	int 	x = 3,
-			*p = NULL;
+	int 	*array = malloc(sizeof(int)),
+			elementCount = 0,
+			input;
 
-	p = &x;
+	printf("Creates a dynamic array. To exit, give number -1.\n");
 
-	printf("%d %d\n", x, *p);
-	printf("%p %p\n", &x, p);
+	while (1) {
+		// Ask for a number
+		printf("Give me a number: ");
+		input = readIntReturn();
+
+		// Break condition
+		if (input == -1)
+			break;
+
+		if (array) {
+			// Add new elements
+			array[elementCount] = input;
+			elementCount++;
+			// Add more space
+			array = (int *)realloc(array, sizeof(int) * (elementCount + 1));
+
+			// Inform user
+			printf(" -> recorded array[%d] = %d\n", elementCount-1, array[elementCount-1]);
+		} else {
+			// We can't add new elements, exit
+			break;
+		}
+	}
+
+	// Free up memory
+	array = realloc(array, 0);
+	
+	// Say bye
+	printf("\nThanks, bye!\n");
 
 	return 0;
 }
